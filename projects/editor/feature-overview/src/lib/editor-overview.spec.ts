b8f20e281de78bef7fa28d0dfc38607fcdc0a3b6
Thomas-P <1452185+Thomas-P@users.noreply.github.com>
2026-02-20 18:26:31 +0100

    feat: implement a basic loading indicator.

--- projects/editor/feature-overview/src/lib/editor-overview.spec.ts
+++ projects/editor/feature-overview/src/lib/editor-overview.spec.ts
@@ -2,11 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditorOverview } from './editor-overview';
 import { provideHttpClient } from '@angular/common/http';
-import {
-  HttpTestingController,
-  provideHttpClientTesting,
-  TestRequest,
-} from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { By } from '@angular/platform-browser';
 
 describe('EditorOverview', () => {
@@ -15,7 +11,7 @@ describe('EditorOverview', () => {
   let httpTestingController: HttpTestingController;
 
   const getLoadingIndicator = () =>
-    fixture.debugElement.query(By.css('[data-automation-id]=pages-are-loading'));
+    fixture.debugElement.query(By.css('[data-automation-id=pages-are-loading]'));
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,25 +22,19 @@ describe('EditorOverview', () => {
     fixture = TestBed.createComponent(EditorOverview);
     component = fixture.componentInstance;
     httpTestingController = TestBed.inject(HttpTestingController);
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('loading pages', () => {
-    let request: TestRequest;
-
-    beforeEach(() => {
-      request = httpTestingController.expectOne('/api/editor/pages');
-    });
-
-    it('should show a loading indicator, while loading the pages', () => {
-      expect(getLoadingIndicator()).toBeTruthy();
-      request.flush([]);
-      fixture.detectChanges();
-      expect(getLoadingIndicator()).toBeFalsy();
-    });
+  it('should show a loading indicator, while loading the pages', async () => {
+    const request = httpTestingController.expectOne('/api/editor/pages');
+    expect(getLoadingIndicator()).toBeTruthy();
+    request.flush([]);
+    await fixture.whenStable();
+    expect(getLoadingIndicator()).toBeFalsy();
+    httpTestingController.verify();
   });
 });
--- projects/editor/feature-overview/src/lib/editor-overview.ts
+++ projects/editor/feature-overview/src/lib/editor-overview.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { editorApi } from '@editor/data';
 
 @Component({
   selector: 'editor-overview',
   imports: [],
-  template: ` <p>feature-overview works!</p> `,
+  templateUrl: 'editor-overview.html',
   styles: ``,
 })
-export class EditorOverview {}
+export class EditorOverview {
+  private readonly editorApi = inject(editorApi);
+  /**
+   * fetches the list of pages
+   */
+  readonly pageRequest = rxResource({
+    stream: () => this.editorApi.listPages(),
+    defaultValue: [],
+  });
+}
